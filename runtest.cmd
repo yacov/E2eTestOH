53bb9b0598915c2c2e883b382342a07f980559a0
Iakov Volf <iakov.volf@isoftbet.com>
2020-10-20 15:29:39 +0300

    update Fill forms script

--- runtest.cmd
+++ runtest.cmd
@@ -2,3 +2,4 @@
 git pull
 npm install cypress --save-dev
 npx cypress run --spec "cypress/integration/FillForm-spec.js" --reporter "progress" --env eventURL=79955cb7-244b-4a0e-8f75-ac5a00c618cb
+@pause
