7e1664dc86d28581c4208c28190abc2b24b35c08
Iakov Volf <iakov.volf@isoftbet.com>
2020-12-03 14:26:06 +0200

    Updated test launcher

--- runtest.cmd
+++ runtest.cmd
@@ -1,5 +1,6 @@
 @ECHO OFF
 git pull
 npm install cypress --save-dev
-npx cypress run --spec "cypress/integration/FillForm-spec.js" --reporter "progress" --env eventURL=79955cb7-244b-4a0e-8f75-ac5a00c618cb
+npx cypress run --spec "cypress/integration/TxSmoke-spec.js" --reporter "teamcity" --record --key "c8c65de2-1a95-4302-8142-727f46d444ba
+" --tag "TxSmoke"
 @pause
